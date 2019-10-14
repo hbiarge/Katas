d261a1cb8fa2170d2f11cae851586380be12d7d3
Hugo Biarge <hbiarge@plainconcepts.com>
2019-10-14 21:09:08 +0200

    Instrument processor kata

---
+++ InstrumentProcessor/InstrumentProcessor/Contracts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InstrumentProcessor
+{
+    public interface ITaskDispatcher
+    {
+        string GetTask();
+        void FinishedTask(string task);
+    }
+
+    public interface IInstrument
+    {
+        void Execute(string task);
+
+        event EventHandler Finished;
+        event EventHandler Error;
+    }
+
+    public interface IInstrumentProcessor
+    {
+        void Process();
+    }
+}
---
+++ InstrumentProcessor/InstrumentProcessor/InstrumentProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace InstrumentProcessor
+{
+    public sealed class InstrumentProcessor : IInstrumentProcessor, IDisposable
+    {
+        private readonly ITaskDispatcher _taskDispatcher;
+        private readonly IInstrument _instrument;
+
+        private string _currentTask;
+
+        public InstrumentProcessor(ITaskDispatcher taskDispatcher, IInstrument instrument)
+        {
+            _taskDispatcher = taskDispatcher;
+            _instrument = instrument;
+
+            _instrument.Finished += InstrumentOnFinished;
+            _instrument.Error += InstrumentOnError;
+        }
+
+        public void Process()
+        {
+            if (_currentTask is null == false)
+            {
+                throw new InvalidOperationException("The instrument is already executing a task");
+            }
+
+            _currentTask = _taskDispatcher.GetTask();
+            _instrument.Execute(_currentTask);
+        }
+
+        public void Dispose()
+        {
+            _instrument.Finished -= InstrumentOnFinished;
+            _instrument.Error -= InstrumentOnError;
+        }
+
+        private void InstrumentOnFinished(object sender, EventArgs e)
+        {
+            _taskDispatcher.FinishedTask(_currentTask);
+            _currentTask = null;
+        }
+
+        private void InstrumentOnError(object sender, EventArgs e)
+        {
+            // Log an error
+            _currentTask = null;
+        }
+    }
+}
